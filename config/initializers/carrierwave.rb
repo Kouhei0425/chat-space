e791a6a58ab0ea4115ea1584fc953c9c3f0da83a
Kouhei0425 <mk.ibra2.swd@ezweb.ne.jp>
2019-11-18 20:55:44 +0900

    a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'kouhei0425'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kouhei0425'
 end
