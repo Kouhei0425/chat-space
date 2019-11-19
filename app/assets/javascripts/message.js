506ff50869a5edd23fcb2f6f75b309cca1cf3968
Kouhei0425 <mk.ibra2.swd@ezweb.ne.jp>
2019-11-19 13:27:21 +0900

    a

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -33,7 +33,8 @@ $(function(){
       contentType: false
     })
     .done(function(message){
-      buildHTML(message);
+      var html = buildHTML(message);
+      $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
       $('form')[0].reset();
     })
